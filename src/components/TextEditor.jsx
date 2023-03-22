d163c85d8ad6b2953c58c511e02ec51d4483ae8c
Rodrigo Casale <popobasura3@hotmail.com>
2023-03-22 15:34:17 -0600

    Text editor style

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -4,31 +4,78 @@ import { Editor } from "react-draft-wysiwyg";
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { Button } from "@chakra-ui/react";
 import { downloadDocx } from "../utils/ExportDocx";
+import "./styles/TextEditor.css";
 
 export default function TextEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
-  const onEditorStateChange = (editorState) => {
-    setEditorState(editorState);
-    console.log(editorState.getCurrentContent().getBlocksAsArray());
-    console.log(editorState.getCurrentContent().getFirstBlock());
-    console.log(editorState.getCurrentContent().getBlockMap().get("text"));
-  };
+
+  const DownloadButton = () => (
+    <Button
+      id="downloadBtn"
+      onClick={() =>
+        downloadDocx(editorState.getCurrentContent().getBlocksAsArray())
+      }
+      value="download"
+      size="sm"
+      variant="ghost"
+      colorScheme="yellow"
+    >
+      Export
+    </Button>
+  );
+
+  const BoldButton = () => (
+    <Button size="sm" variant="ghost" colorScheme="yellow">
+      B
+    </Button>
+  );
   return (
     <>
-      <Button
-        id="downloadBtn"
-        onClick={() =>
-          downloadDocx(editorState.getCurrentContent().getBlocksAsArray())
-        }
-        value="download"
-      >
-        Export
-      </Button>
       <Editor
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        wrapperClassName="wrapper-class"
         editorState={editorState}
-        onEditorStateChange={onEditorStateChange}
+        onEditorStateChange={setEditorState}
+        toolbarCustomButtons={[<DownloadButton />]}
+        toolbar={{
+          options: ["inline", "blockType", "fontSize", "fontFamily"],
+          blockType: {
+            options: ["Normal", "H1", "H2", "H3", "H4", "H5", "H6"],
+            className: "bordered-dropdown-classname",
+          },
+          fontFamily: {
+            options: [
+              "Arial",
+              "Georgia",
+              "Impact",
+              "Tahoma",
+              "Times New Roman",
+              "Verdana",
+            ],
+            className: "bordered-dropdown-classname",
+          },
+          fontSize: {
+            options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+            className: "bordered-dropdown-classname",
+          },
+          inline: {
+            options: [
+              "bold",
+              "italic",
+              "underline",
+              "strikethrough",
+              "monospace",
+            ],
+            bold: { className: "bordered-option-classname" },
+            italic: { className: "bordered-option-classname" },
+            underline: { className: "bordered-option-classname" },
+            strikethrough: { className: "bordered-option-classname" },
+            monospace: { className: "bordered-option-classname" },
+          },
+        }}
       />
     </>
   );
